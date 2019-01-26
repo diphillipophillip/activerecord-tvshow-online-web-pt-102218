f5c41197e8836696a80a585a5b57a7ba0c32e700
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-26 22:03:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,46 @@
 class Show < ActiveRecord::Base
+  
+  def Show.highest_rating 
+    show = Show.maximum("rating")
+    show 
+  end 
+  
+  def Show.most_popular_show 
+    show = self.highest_rating 
+    Show.all.find do |i| 
+      if i.rating == show 
+        i 
+      end 
+    end 
+  end 
+  
+  def Show.lowest_rating 
+    show = Show.minimum("rating")
+    show
+  end 
+  
+  def Show.least_popular_show
+    show = self.lowest_rating 
+    Show.all.find do |i| 
+      if i.rating == show 
+        i 
+      end 
+    end 
+  end   
+  
+  def Show.ratings_sum 
+    Show.sum("rating")
+  end 
+  
+  def Show.popular_shows
+    Show.all.map do |i| 
+      if i.rating >= 5 
+        i 
+      end
+    end 
+  end 
+  
+  
+  
+  
 end 
